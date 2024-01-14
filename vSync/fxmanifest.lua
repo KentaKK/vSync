ece6ef5128108be16573ef7b34d921746ebac832
Kenta <102387086+KentaKK@users.noreply.github.com>
2024-01-14 20:38:56 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ vSync/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+
+shared_scripts {
+    '@es_extended/imports.lua'
+}
+
+client_scripts {
+    'vs_client.lua'
+}
+
+server_scripts {
+    'vs_server.lua'
+}
